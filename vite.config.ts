c53ea72019437ef4ac9b0c707770bd4e4a1d9cc3
koolkat254 <phill_hodges@hotmail.com>
2026-01-31 10:48:49 -0600

    Update base path for renamed repo QRScoutFBI

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QRScout2025/',
+  base: '/QRScoutFBI/',
   plugins: [react()],
   resolve: {
     alias: {
